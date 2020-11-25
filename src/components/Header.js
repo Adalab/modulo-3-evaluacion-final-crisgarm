c5222e2ed03eff7e30ca0bdb9970a8d9e4210dbf
crisgarm <crisgarm9@gmail.com>
2020-11-25 13:37:55 +0100

    Add audio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../images/logo-rick-morty.png";
 import "../stylesheets/Header.scss";
+import audio from "../audio/rick-and-morty-theme-song-hd.mp3";
 
 const Header = () => {
   return (
@@ -11,6 +12,9 @@ const Header = () => {
         title="Logo de Rick and Morty"
         className="header__image"
       />
+      <audio className="header__audio" autoplay controls loop>
+        <source src={audio} type="audio/mp3" />
+      </audio>
     </header>
   );
 };
